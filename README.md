d80b53e396c27726bc27816509b8d64ec44303de
przydatek@github.com <bartosz.przydatek@dfinity.org>
2024-02-29 11:55:51 +0100

    Add issuer BE code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # early-adopter-issuer
-An implementation of a VC issuer for "VerifiedEarlyAdopter" credentials
+An implementation of a VC issuer for "VerifiedEarlyAdopter" credentials.
+
+## Testing
+
+To run tests via `cargo test` two binaries are needed, namely `ic-test-state-machine` and `internet_identity.wasm.gz`, 
+whose location should be set via environment variables `STATE_MACHINE_BINARY` resp. `II_WASM`.
---
+++ src/main.rs
@@ -0,0 +1,562 @@
+use candid::{candid_method, CandidType, Deserialize, Principal};
+use canister_sig_util::signature_map::{SignatureMap, LABEL_SIG};
+use canister_sig_util::{extract_raw_root_pk_from_der, CanisterSigPublicKey, IC_ROOT_PK_DER};
+use ic_cdk::api::{caller, set_certified_data, time};
+use ic_cdk_macros::{init, query, update};
+use ic_certification::{fork_hash, labeled_hash, pruned, Hash};
+use ic_stable_structures::storable::Bound;
+use ic_stable_structures::{DefaultMemoryImpl, RestrictedMemory, StableCell, Storable};
+use include_dir::{include_dir, Dir};
+use serde_bytes::ByteBuf;
+use sha2::{Digest, Sha256};
+use std::borrow::Cow;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use vc_util::issuer_api::{
+    ArgumentValue, CredentialSpec, GetCredentialRequest, Icrc21ConsentInfo, Icrc21Error,
+    Icrc21ErrorInfo, Icrc21VcConsentMessageRequest, IssueCredentialError, IssuedCredentialData,
+    PrepareCredentialRequest, PreparedCredentialData, SignedIdAlias,
+};
+use vc_util::{
+    build_credential_jwt, did_for_principal, get_verified_id_alias_from_jws, vc_jwt_to_jws,
+    vc_signing_input, vc_signing_input_hash, AliasTuple, CredentialParams,
+};
+
+use asset_util::{collect_assets, CertifiedAssets};
+use ic_cdk::api;
+use ic_cdk_macros::post_upgrade;
+
+/// We use restricted memory in order to ensure the separation between non-managed config memory (first page)
+/// and the managed memory for potential other data of the canister.
+type Memory = RestrictedMemory<DefaultMemoryImpl>;
+type ConfigCell = StableCell<IssuerConfig, Memory>;
+
+const ISSUER_URL: &str = "https://internetidentity.vc";
+pub const CREDENTIAL_URL_PREFIX: &str = "data:text/plain;charset=UTF-8,";
+
+const MINUTE_NS: u64 = 60 * 1_000_000_000;
+const PROD_II_CANISTER_ID: &str = "rdmx6-jaaaa-aaaaa-aaadq-cai";
+// The expiration of issued verifiable credentials.
+const VC_EXPIRATION_PERIOD_NS: u64 = 15 * MINUTE_NS;
+// End of year 2024 as UNIX timestamp.
+const EOY_2024_TIMESTAMP_S: u32 = 1735685999;
+
+// Internal container of per-user data.
+struct EarlyAdopterData {
+    pub joined_timestamp_s: u32,
+}
+
+// User-facing container of per-user data.
+#[derive(CandidType, Deserialize)]
+pub struct EarlyAdopterStatus {
+    pub joined_timestamp_s: u32,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum EarlyAdopterError {
+    Internal(String),
+}
+
+thread_local! {
+    /// Static configuration of the canister set by init() or post_upgrade().
+    static CONFIG: RefCell<ConfigCell> = RefCell::new(ConfigCell::init(config_memory(), IssuerConfig::default()).expect("failed to initialize stable cell"));
+    static SIGNATURES : RefCell<SignatureMap> = RefCell::new(SignatureMap::default());
+    static EARLY_ADOPTERS : RefCell<HashMap<Principal, EarlyAdopterData>> = RefCell::new(HashMap::new());
+
+    // Assets for the management app
+    static ASSETS: RefCell<CertifiedAssets> = RefCell::new(CertifiedAssets::default());
+}
+
+/// Reserve the first stable memory page for the configuration stable cell.
+fn config_memory() -> Memory {
+    RestrictedMemory::new(DefaultMemoryImpl::default(), 0..1)
+}
+
+#[cfg(target_arch = "wasm32")]
+use ic_cdk::println;
+
+#[derive(CandidType, Deserialize)]
+struct IssuerConfig {
+    /// Root of trust for checking canister signatures.
+    ic_root_key_raw: Vec<u8>,
+    /// List of canister ids that are allowed to provide id alias credentials.
+    idp_canister_ids: Vec<Principal>,
+}
+
+impl Storable for IssuerConfig {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(candid::encode_one(self).expect("failed to encode IssuerConfig"))
+    }
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        candid::decode_one(&bytes).expect("failed to decode IssuerConfig")
+    }
+    const BOUND: Bound = Bound::Unbounded;
+}
+
+impl Default for IssuerConfig {
+    fn default() -> Self {
+        Self {
+            ic_root_key_raw: extract_raw_root_pk_from_der(IC_ROOT_PK_DER)
+                .expect("failed to extract raw root pk from der"),
+            idp_canister_ids: vec![Principal::from_text(PROD_II_CANISTER_ID).unwrap()],
+        }
+    }
+}
+
+impl From<IssuerInit> for IssuerConfig {
+    fn from(init: IssuerInit) -> Self {
+        Self {
+            ic_root_key_raw: extract_raw_root_pk_from_der(&init.ic_root_key_der)
+                .expect("failed to extract raw root pk from der"),
+            idp_canister_ids: init.idp_canister_ids,
+        }
+    }
+}
+
+#[derive(CandidType, Deserialize)]
+struct IssuerInit {
+    /// Root of trust for checking canister signatures.
+    ic_root_key_der: Vec<u8>,
+    /// List of canister ids that are allowed to provide id alias credentials.
+    idp_canister_ids: Vec<Principal>,
+}
+
+#[init]
+#[candid_method(init)]
+fn init(init_arg: Option<IssuerInit>) {
+    if let Some(init) = init_arg {
+        apply_config(init);
+    };
+
+    init_assets();
+}
+
+#[post_upgrade]
+fn post_upgrade(init_arg: Option<IssuerInit>) {
+    init(init_arg);
+}
+
+#[update]
+#[candid_method]
+fn configure(config: IssuerInit) {
+    apply_config(config);
+}
+
+fn apply_config(init: IssuerInit) {
+    CONFIG
+        .with_borrow_mut(|config_cell| config_cell.set(IssuerConfig::from(init)))
+        .expect("failed to apply issuer config");
+}
+
+fn authorize_vc_request(
+    alias: &SignedIdAlias,
+    expected_vc_subject: &Principal,
+    current_time_ns: u128,
+) -> Result<AliasTuple, IssueCredentialError> {
+    CONFIG.with_borrow(|config| {
+        let config = config.get();
+
+        for idp_canister_id in &config.idp_canister_ids {
+            if let Ok(alias_tuple) = get_verified_id_alias_from_jws(
+                &alias.credential_jws,
+                expected_vc_subject,
+                idp_canister_id,
+                &config.ic_root_key_raw,
+                current_time_ns,
+            ) {
+                return Ok(alias_tuple);
+            }
+        }
+        Err(IssueCredentialError::InvalidIdAlias(
+            "id alias could not be verified".to_string(),
+        ))
+    })
+}
+
+#[update]
+#[candid_method]
+async fn prepare_credential(
+    req: PrepareCredentialRequest,
+) -> Result<PreparedCredentialData, IssueCredentialError> {
+    let alias_tuple = match authorize_vc_request(&req.signed_id_alias, &caller(), time().into()) {
+        Ok(alias_tuple) => alias_tuple,
+        Err(err) => return Err(err),
+    };
+
+    let credential_jwt = match prepare_credential_jwt(&req.credential_spec, &alias_tuple) {
+        Ok(credential) => credential,
+        Err(err) => return Result::<PreparedCredentialData, IssueCredentialError>::Err(err),
+    };
+    let seed = calculate_seed(&alias_tuple.id_alias);
+    let canister_id = ic_cdk::id();
+    let canister_sig_pk = CanisterSigPublicKey::new(canister_id, seed.to_vec());
+    let signing_input =
+        vc_signing_input(&credential_jwt, &canister_sig_pk).expect("failed getting signing_input");
+    let msg_hash = vc_signing_input_hash(&signing_input);
+
+    SIGNATURES.with(|sigs| {
+        let mut sigs = sigs.borrow_mut();
+        sigs.add_signature(seed.as_ref(), msg_hash);
+    });
+    update_root_hash();
+    Ok(PreparedCredentialData {
+        prepared_context: Some(ByteBuf::from(credential_jwt.as_bytes())),
+    })
+}
+
+fn update_root_hash() {
+    SIGNATURES.with_borrow(|sigs| {
+        ASSETS.with_borrow(|assets| {
+            let prefixed_root_hash = fork_hash(
+                // NB: Labels added in lexicographic order.
+                &assets.root_hash(),
+                &labeled_hash(LABEL_SIG, &sigs.root_hash()),
+            );
+
+            set_certified_data(&prefixed_root_hash[..]);
+        })
+    })
+}
+
+#[query]
+#[candid_method(query)]
+fn get_credential(req: GetCredentialRequest) -> Result<IssuedCredentialData, IssueCredentialError> {
+    let alias_tuple = match authorize_vc_request(&req.signed_id_alias, &caller(), time().into()) {
+        Ok(alias_tuple) => alias_tuple,
+        Err(err) => return Result::<IssuedCredentialData, IssueCredentialError>::Err(err),
+    };
+    if let Err(err) = verify_early_adopter_spec_and_get_since_year(&req.credential_spec) {
+        return Result::<IssuedCredentialData, IssueCredentialError>::Err(
+            IssueCredentialError::UnsupportedCredentialSpec(err),
+        );
+    }
+    let subject_principal = alias_tuple.id_alias;
+    let seed = calculate_seed(&subject_principal);
+    let canister_id = ic_cdk::id();
+    let canister_sig_pk = CanisterSigPublicKey::new(canister_id, seed.to_vec());
+    let prepared_context = match req.prepared_context {
+        Some(context) => context,
+        None => {
+            return Result::<IssuedCredentialData, IssueCredentialError>::Err(internal_error(
+                "missing prepared_context",
+            ))
+        }
+    };
+    let credential_jwt = match String::from_utf8(prepared_context.into_vec()) {
+        Ok(s) => s,
+        Err(_) => {
+            return Result::<IssuedCredentialData, IssueCredentialError>::Err(internal_error(
+                "invalid prepared_context",
+            ))
+        }
+    };
+    let signing_input =
+        vc_signing_input(&credential_jwt, &canister_sig_pk).expect("failed getting signing_input");
+    let message_hash = vc_signing_input_hash(&signing_input);
+    let sig_result = SIGNATURES.with(|sigs| {
+        let sig_map = sigs.borrow();
+        let certified_assets_root_hash = ASSETS.with_borrow(|assets| assets.root_hash());
+        sig_map.get_signature_as_cbor(&seed, message_hash, Some(certified_assets_root_hash))
+    });
+    let sig = match sig_result {
+        Ok(sig) => sig,
+        Err(e) => {
+            return Result::<IssuedCredentialData, IssueCredentialError>::Err(
+                IssueCredentialError::SignatureNotFound(format!(
+                    "signature not prepared or expired: {}",
+                    e
+                )),
+            );
+        }
+    };
+    let vc_jws =
+        vc_jwt_to_jws(&credential_jwt, &canister_sig_pk, &sig).expect("failed constructing JWS");
+    Result::<IssuedCredentialData, IssueCredentialError>::Ok(IssuedCredentialData { vc_jws })
+}
+
+#[update]
+#[candid_method]
+async fn vc_consent_message(
+    req: Icrc21VcConsentMessageRequest,
+) -> Result<Icrc21ConsentInfo, Icrc21Error> {
+    get_vc_consent_message_en(&req.credential_spec)
+}
+
+const EARLY_ADOPTER_VC_CONSENT_EN: &str = r###"# Verifiable Credentials Early Adopter
+
+Credential stating that you are an Early Adopter of VC-tech since at least"###;
+
+pub fn get_vc_consent_message_en(
+    credential_spec: &CredentialSpec,
+) -> Result<Icrc21ConsentInfo, Icrc21Error> {
+    match verify_early_adopter_spec_and_get_since_year(credential_spec) {
+        Err(err) => Err(Icrc21Error::ConsentMessageUnavailable(Icrc21ErrorInfo {
+            description: err,
+        })),
+        Ok(since_year) => Ok(Icrc21ConsentInfo {
+            consent_message: format!("{} {}.", EARLY_ADOPTER_VC_CONSENT_EN, since_year),
+            language: "en".to_string(),
+        }),
+    }
+}
+
+// Note: not very accurate, but we cannot depend on `chrono` due to WASM-ability.
+// Assumption: `year` is at least 2024 (as checked before calling this function)
+fn year_to_max_timestamp_s(year: i32) -> u32 {
+    const SECONDS_IN_YEAR: u32 = 31_536_000;
+    EOY_2024_TIMESTAMP_S + ((year - 2024) as u32) * SECONDS_IN_YEAR
+}
+
+fn verify_early_adopter_spec_and_get_since_year(spec: &CredentialSpec) -> Result<i32, String> {
+    if spec.credential_type.as_str() == "EarlyAdopter" {
+        let Some(arguments) = &spec.arguments else {
+            return Err("Credential spec has no arguments".to_string());
+        };
+        let expected_argument = "sinceYear";
+        let Some(value) = arguments.get(expected_argument) else {
+            return Err(format!(
+                "Credential spec has no {}-argument",
+                expected_argument
+            ));
+        };
+        if arguments.len() != 1 {
+            return Err("Credential spec has unexpected arguments".to_string());
+        }
+        let ArgumentValue::Int(year) = value else {
+            return Err(format!(
+                "Credential spec has unexpected value for {}-argument",
+                expected_argument
+            ));
+        };
+        if *year < 2024 {
+            return Err(format!(
+                "Credential spec has unsupported value for {}-argument",
+                expected_argument
+            ));
+        };
+
+        Ok(*year)
+    } else {
+        Err(format!(
+            "Credential {} is not supported",
+            spec.credential_type.as_str()
+        ))
+    }
+}
+
+#[update]
+#[candid_method]
+fn register_early_adopter() -> Result<EarlyAdopterStatus, EarlyAdopterError> {
+    let user_id = caller();
+    let joined_timestamp_s = (time() / 1_000_000_000) as u32;
+    EARLY_ADOPTERS.with_borrow_mut(|adopters| {
+        adopters.insert(user_id, EarlyAdopterData { joined_timestamp_s })
+    });
+    println!(
+        "Registered {} at timestamp {}.",
+        user_id.to_text(),
+        joined_timestamp_s
+    );
+    Ok(EarlyAdopterStatus { joined_timestamp_s })
+}
+
+#[query]
+#[candid_method(query)]
+pub fn http_request(req: HttpRequest) -> HttpResponse {
+    let parts: Vec<&str> = req.url.split('?').collect();
+    let path = parts[0];
+    let sigs_root_hash =
+        SIGNATURES.with_borrow(|sigs| pruned(labeled_hash(LABEL_SIG, &sigs.root_hash())));
+    let maybe_asset = ASSETS.with_borrow(|assets| {
+        assets.get_certified_asset(path, req.certificate_version, Some(sigs_root_hash))
+    });
+
+    let mut headers = static_headers();
+    match maybe_asset {
+        Some(asset) => {
+            headers.extend(asset.headers);
+            HttpResponse {
+                status_code: 200,
+                body: ByteBuf::from(asset.content),
+                headers,
+            }
+        }
+        None => HttpResponse {
+            status_code: 404,
+            headers,
+            body: ByteBuf::from(format!("Asset {} not found.", path)),
+        },
+    }
+}
+
+fn static_headers() -> Vec<(String, String)> {
+    vec![("Access-Control-Allow-Origin".to_string(), "*".to_string())]
+}
+
+fn main() {}
+
+fn calculate_seed(principal: &Principal) -> Hash {
+    // IMPORTANT: In a real dapp the salt should be set to a random value.
+    let dummy_salt = [5u8; 32];
+
+    let mut bytes: Vec<u8> = vec![];
+    bytes.push(dummy_salt.len() as u8);
+    bytes.extend_from_slice(&dummy_salt);
+
+    let principal_bytes = principal.as_slice();
+    bytes.push(principal_bytes.len() as u8);
+    bytes.extend(principal_bytes);
+    hash_bytes(bytes)
+}
+
+fn verified_early_adopter_credential(
+    subject_principal: Principal,
+    credential_spec: &CredentialSpec,
+) -> String {
+    let params = CredentialParams {
+        spec: credential_spec.clone(),
+        subject_id: did_for_principal(subject_principal),
+        credential_id_url: credential_id_for_principal(subject_principal),
+        issuer_url: ISSUER_URL.to_string(),
+        expiration_timestamp_s: exp_timestamp_s(),
+    };
+    build_credential_jwt(params)
+}
+
+fn exp_timestamp_s() -> u32 {
+    ((time() + VC_EXPIRATION_PERIOD_NS) / 1_000_000_000) as u32
+}
+
+// Prepares a unique id for the given subject_principal.
+// The returned URL has the format: "data:text/plain;charset=UTF-8,issuer:...,timestamp_ns:...,subject:..."
+fn credential_id_for_principal(subject_principal: Principal) -> String {
+    let issuer = format!("issuer:{}", ISSUER_URL);
+    let timestamp = format!("timestamp_ns:{}", time());
+    let subject = format!("subject:{}", subject_principal.to_text());
+    format!(
+        "{}{},{},{}",
+        CREDENTIAL_URL_PREFIX, issuer, timestamp, subject
+    )
+}
+
+fn prepare_credential_jwt(
+    credential_spec: &CredentialSpec,
+    alias_tuple: &AliasTuple,
+) -> Result<String, IssueCredentialError> {
+    let since_year = verify_early_adopter_spec_and_get_since_year(credential_spec)
+        .map_err(|e| IssueCredentialError::UnsupportedCredentialSpec(e))?;
+    let max_timestamp_s = year_to_max_timestamp_s(since_year);
+    EARLY_ADOPTERS.with_borrow(|adopters| {
+        verify_principal_registered_and_authorized(alias_tuple.id_dapp, adopters, max_timestamp_s)
+    })?;
+    Ok(verified_early_adopter_credential(
+        alias_tuple.id_alias,
+        credential_spec,
+    ))
+}
+
+fn verify_principal_registered_and_authorized(
+    user: Principal,
+    adopters: &HashMap<Principal, EarlyAdopterData>,
+    max_timestamp: u32,
+) -> Result<(), IssueCredentialError> {
+    let Some(ea_data) = adopters.get(&user) else {
+        println!(
+            "*** principal {} it is not registered for early adopter credential",
+            user.to_text(),
+        );
+        return Err(IssueCredentialError::UnauthorizedSubject(format!(
+            "unregistered principal {}",
+            user.to_text()
+        )));
+    };
+    if ea_data.joined_timestamp_s < max_timestamp {
+        Ok(())
+    } else {
+        println!(
+            "*** principal {} it is not authorized for early adopter credential",
+            user.to_text(),
+        );
+        Err(IssueCredentialError::UnauthorizedSubject(format!(
+            "unauthorized principal {}",
+            user.to_text()
+        )))
+    }
+}
+
+fn internal_error(msg: &str) -> IssueCredentialError {
+    IssueCredentialError::Internal(String::from(msg))
+}
+
+fn hash_bytes(value: impl AsRef<[u8]>) -> Hash {
+    let mut hasher = Sha256::new();
+    hasher.update(value.as_ref());
+    hasher.finalize().into()
+}
+
+// Order dependent: do not move above any function annotated with #[candid_method]!
+candid::export_service!();
+
+// Assets
+static ASSET_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/dist");
+pub fn init_assets() {
+    ASSETS.with_borrow_mut(|assets| {
+        *assets = CertifiedAssets::certify_assets(
+            collect_assets(&ASSET_DIR, Some(fixup_html)),
+            &static_headers(),
+        );
+    });
+
+    update_root_hash()
+}
+pub type HeaderField = (String, String);
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub struct HttpRequest {
+    pub method: String,
+    pub url: String,
+    pub headers: Vec<HeaderField>,
+    pub body: ByteBuf,
+    pub certificate_version: Option<u16>,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub struct HttpResponse {
+    pub status_code: u16,
+    pub headers: Vec<HeaderField>,
+    pub body: ByteBuf,
+}
+
+fn fixup_html(html: &str) -> String {
+    let canister_id = api::id();
+
+    // the string we are replacing here is inserted by vite during the front-end build
+    html.replace(
+            r#"<script type="module" crossorigin src="/index.js"></script>"#,
+            &format!(r#"<script data-canister-id="{canister_id}" type="module" crossorigin src="/index.js"></script>"#).to_string()
+        )
+}
+
+#[cfg(test)]
+mod test {
+    use crate::__export_service;
+    use candid_parser::utils::{service_equal, CandidSource};
+    use std::path::Path;
+
+    /// Checks candid interface type equality by making sure that the service in the did file is
+    /// equal to the generated interface.
+    #[test]
+    fn check_candid_interface_compatibility() {
+        let canister_interface = __export_service();
+        service_equal(
+            CandidSource::Text(&canister_interface),
+            CandidSource::File(Path::new("early_adopter_issuer.did")),
+        )
+        .unwrap_or_else(|e| {
+            panic!(
+                "the canister code interface is not equal to the did file: {:?}",
+                e
+            )
+        });
+    }
+}
