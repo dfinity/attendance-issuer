a2e30974d2a08a198eefabf3ddb6b0384ab25a3c
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-07 17:41:15 +0100

    Add testInfo

--- frontend/e2e-tests/registration.spec.ts
+++ frontend/e2e-tests/registration.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { signInWithNewUser } from './utils';
 
-test("use registers with Internet Identity and is redirected to a success page", async ({ page, context }) => {
+test("use registers with Internet Identity and is redirected to a success page", async ({ page, context }, testInfo) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/Early Adopter Campaign/);
 
-  await signInWithNewUser({ page, context });
+  await signInWithNewUser({ page, context, testInfo });
   
   expect(await page.getByTestId("success-page-title").textContent()).toBe("Congratulations!");
 });
