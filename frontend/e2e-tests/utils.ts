601a4396014613f8698915d49b37b528e75e962b
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-07 20:37:03 +0100

    Add waitFor

--- frontend/e2e-tests/utils.ts
+++ frontend/e2e-tests/utils.ts
@@ -19,9 +19,10 @@ export const signInWithNewUser = async ({
   await iiPage.locator("#registerButton").click();
   await iiPage.locator("[data-action=construct-identity]").click();
 
+  await iiPage.waitForTimeout(10_000);
+  await iiPage.locator("input#captchaInput").waitFor();
   const screenshot = await iiPage.screenshot();
   await testInfo.attach('screenshot', { body: screenshot, contentType: 'image/png' });
-  await iiPage.locator("input#captchaInput").waitFor();
   await iiPage.locator("input#captchaInput").fill("a");
   await iiPage.locator("#confirmRegisterButton").click();
 
