d435cd2950fe9fca7cad0927f8e5ab449f2b5c59
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-07 14:39:43 +0100

    Add e2e test to CI

---
+++ scripts/create-env-vars.sh
@@ -0,0 +1,22 @@
+# This script creates the `.env` file and populates it with the environment variables depending on the environment.
+
+test -n "$DFX_NETWORK" # Will fail if not defined.
+export DFX_NETWORK
+
+ENV_FILE=${ENV_OUTPUT_FILE:-$PWD/frontend/.env}
+
+II_CANISTER_ID=$(dfx canister id internet_identity --network "$DFX_NETWORK")
+EARLY_CANISTER_ID=$(dfx canister id early_adopter --network "$DFX_NETWORK")
+
+if [ "$DFX_NETWORK" = "local" ]; then
+  REPLICA_SERVER_PORT=$(dfx info webserver-port)
+  II_URL="http://${II_CANISTER_ID}.localhost:8080"
+  HOST="http://localhost:${REPLICA_SERVER_PORT}"
+  echo "PUBLIC_INTERNET_IDENTITY_URL=${II_URL}\nPUBLIC_HOST=${HOST}\nPUBLIC_FETCH_ROOT_KEY=true\n" > $ENV_FILE
+fi
+if [ "$DFX_NETWORK" = "mainnet" ]; then
+  II_URL="https://${II_CANISTER_ID}.ic0.app"
+  HOST="https://icp-api.io"
+  echo "PUBLIC_INTERNET_IDENTITY_URL=${II_URL}\nPUBLIC_HOST=${HOST}\nPUBLIC_FETCH_ROOT_KEY=false\n" > $ENV_FILE
+fi
+
