f67ab12e227f46680ead3f582238b673aa59541d
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-12 14:44:49 +0100

    Add extra checks

--- build-dummy-rp.sh
+++ build-dummy-rp.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Make sure we always run from the issuer root
+EARLY_ADOPTER_ISSUER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$EARLY_ADOPTER_ISSUER_DIR"
+
 # Build the frontend
 cd dummy-relying-party/
 npm ci
