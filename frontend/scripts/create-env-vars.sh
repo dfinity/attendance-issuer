14064881fd9a424babc810f7ef0b34e5de0b6ba4
Llorenç <llorenc.muntaner@dfinity.org>
2024-05-17 09:58:16 +0200

    Setup Juno analytics

--- frontend/scripts/create-env-vars.sh
+++ frontend/scripts/create-env-vars.sh
@@ -36,10 +36,12 @@ if [ "$DFX_NETWORK" = "ic_test" ]; then
   echo "PUBLIC_FETCH_ROOT_KEY=false" >> $ENV_FILE
 fi
 if [ "$DFX_NETWORK" = "mainnet" ]; then
-  II_URL="https://${II_CANISTER_ID}.ic0.app"
+  II_URL="https://identity.ic0.app"
   HOST="https://icp-api.io"
   echo "PUBLIC_INTERNET_IDENTITY_URL=${II_URL}" > $ENV_FILE
   echo "PUBLIC_HOST=${HOST}" >> $ENV_FILE
   echo "PUBLIC_FETCH_ROOT_KEY=false" >> $ENV_FILE
+  echo "PUBLIC_JUNO_SATELLITE_ID=j62zl-viaaa-aaaal-ajfcq-cai" > $ENV_FILE
+  echo "PUBLIC_JUNO_ORBITER_ID=sfdba-gaaaa-aaaal-ad5ua-cai" > $ENV_FILE
 fi
 
