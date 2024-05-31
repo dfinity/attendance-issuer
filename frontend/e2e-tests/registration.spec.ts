fef939a489c39877bf9e1745b8e52501252ded13
Llorenç <llorenc.muntaner@dfinity.org>
2024-05-31 14:33:23 +0200

    Fix e2e test

--- frontend/e2e-tests/registration.spec.ts
+++ frontend/e2e-tests/registration.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { signInWithNewUser } from './utils';
 
+// Needs to match the event created in the backend.
 const eventName = "TEST";
-const eventCode = "TESTCODE"
+const eventCode = "testcode"
 
 test("use registers with Internet Identity and is redirected to a success page", async ({ page, context }) => {
   await page.goto(`/?e=${eventName}&c=${eventCode}`);
