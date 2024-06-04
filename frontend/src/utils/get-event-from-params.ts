ee7144b90c9251d61cd7c59f3c93a069408ac4c9
Llorenç <llorenc.muntaner@dfinity.org>
2024-06-04 09:53:32 +0200

    Fix e2e

--- frontend/src/utils/get-event-from-params.ts
+++ frontend/src/utils/get-event-from-params.ts
@@ -4,12 +4,12 @@ export const getEventFromParams = () => {
   const url = new URL(window.location.href);
   const params = url.searchParams;
   const eventName = params.get(EVENT_NAME_PARAM_KEY);
-  const code = params.get(EVENT_CODE_PARAM_KEY);
-  if (!eventName || !code) {
+  const registrationCode = params.get(EVENT_CODE_PARAM_KEY);
+  if (!eventName || !registrationCode) {
     return undefined;
   }
   return {
     eventName,
-    code
+    registrationCode,
   }
 };
